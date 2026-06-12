48d27fbaeeb3f957dd742cfb24f0b007109f1320
Matheus Motta Soriano <matheusmottasoriano@gmail.com>
2026-06-11 21:30:53 -0300

    criar modelo de item

---
+++ lib/item_cafe.dart
@@ -0,0 +1,13 @@
+class CafeItem {
+  final String nome;
+  final double preco;
+  final String imagemUrl;
+  final String categoria;
+
+  const CafeItem({
+    required this.nome,
+    required this.preco,
+    required this.imagemUrl,
+    required this.categoria,
+  });
+}
